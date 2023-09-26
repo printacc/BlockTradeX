9d02d2622476d02dbe2356fc673927cee532388e
yangqing <2066268357@qq.com>
2023-09-26 23:41:56 +0800

    聚合工程

---
+++ nucleus/trade-center/src/main/java/cn/muses/trade/TradeCenterApplication.java
@@ -0,0 +1,15 @@
+package cn.muses.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TradeCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeCenterApplication.class, args);
+    }
+
+}
