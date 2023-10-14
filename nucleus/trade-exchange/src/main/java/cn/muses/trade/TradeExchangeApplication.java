162c95ddd3bf58957940aa459c63cef445800e7a
yangqing <2066268357@qq.com>
2023-10-14 12:58:01 +0800

    exchange模块

---
+++ nucleus/trade-exchange/src/main/java/cn/muses/trade/TradeExchangeApplication.java
@@ -0,0 +1,13 @@
+package cn.muses.trade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeExchangeApplication.class, args);
+    }
+
+}
---
+++ nucleus/trade-exchange/src/test/java/cn/muses/trade/TradeExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.muses.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
