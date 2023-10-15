6daeb2b6ebe8e9e9cd890015791a1ca54c8e42c2
yangqing <2066268357@qq.com>
2023-10-15 23:58:42 +0800

    usdt钱包

---
+++ trade_wallet_rpc/wallet-btc-usdt/src/main/java/cn/muses/wallet/WalletBtcUsdtApplication.java
@@ -0,0 +1,13 @@
+package cn.muses.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletBtcUsdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletBtcUsdtApplication.class, args);
+    }
+
+}
