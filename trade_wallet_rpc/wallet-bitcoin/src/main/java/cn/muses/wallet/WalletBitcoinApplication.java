4c06c6960a6d23f0f2b3194b2b9e778c593cd0cc
yangqing <2066268357@qq.com>
2023-10-15 23:57:07 +0800

    bitcoin钱包

---
+++ trade_wallet_rpc/wallet-bitcoin/src/main/java/cn/muses/wallet/WalletBitcoinApplication.java
@@ -0,0 +1,13 @@
+package cn.muses.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletBitcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletBitcoinApplication.class, args);
+    }
+
+}
