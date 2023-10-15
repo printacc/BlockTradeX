e8d12e392f31ec27235895d81b5b80fa0b2a297d
yangqing <2066268357@qq.com>
2023-10-16 00:00:18 +0800

    eth钱包

---
+++ trade_wallet_rpc/wallet-eth/src/main/java/cn/muses/wallet/WalletEthApplication.java
@@ -0,0 +1,13 @@
+package cn.muses.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletEthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletEthApplication.class, args);
+    }
+
+}
