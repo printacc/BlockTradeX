dddd076e7dbd4ae33431eaaffdb5b413e5ef1c7d
yangqing <2066268357@qq.com>
2023-10-16 00:05:39 +0800

    rpc连接公用模块

---
+++ trade_wallet_rpc/wallet-rpc-common/src/test/java/cn/muses/wallet/WalletRpcCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.muses.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletRpcCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
