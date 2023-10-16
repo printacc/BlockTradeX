2476c0c4363af1c4c4e28eb61122972cdc6d2bc3
yangqing <2066268357@qq.com>
2023-10-16 14:53:08 +0800

    bitcoin-usdt设置

--- trade_wallet_rpc/wallet-bitcoin/src/main/java/cn/muses/wallet/WalletBitcoinApplication.java
+++ trade_wallet_rpc/wallet-bitcoin/src/main/java/cn/muses/wallet/WalletBitcoinApplication.java
@@ -2,8 +2,10 @@ package cn.muses.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WalletBitcoinApplication {
 
     public static void main(String[] args) {
--- trade_wallet_rpc/wallet-btc-usdt/src/main/java/cn/muses/wallet/WalletBtcUsdtApplication.java
+++ trade_wallet_rpc/wallet-btc-usdt/src/main/java/cn/muses/wallet/WalletBtcUsdtApplication.java
@@ -2,8 +2,12 @@ package cn.muses.wallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class WalletBtcUsdtApplication {
 
     public static void main(String[] args) {
