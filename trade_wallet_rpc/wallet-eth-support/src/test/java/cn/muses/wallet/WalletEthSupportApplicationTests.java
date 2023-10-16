64475dc8fad5648bedc5170eafa0b129628e70b2
yangqing <2066268357@qq.com>
2023-10-16 20:48:12 +0800

    ETH设置

---
+++ trade_wallet_rpc/wallet-eth-support/src/test/java/cn/muses/wallet/WalletEthSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.muses.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletEthSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ trade_wallet_rpc/wallet-eth/src/main/java/cn/muses/wallet/job/CoinCollectJob.java
@@ -0,0 +1,43 @@
+package cn.muses.wallet.job;
+
+
+import cn.muses.wallet.service.AccountService;
+import cn.muses.wallet.service.EthService;
+import cn.muses.wallet.util.AccountReplay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class CoinCollectJob {
+    private Logger logger = LoggerFactory.getLogger(CoinCollectJob.class);
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private EthService ethService;
+
+    /**
+     * 同步ETH地址余额
+     */
+//    @Scheduled(cron = "0 0 */2 * * *")
+    @Scheduled(cron = "0 0 17 * * ?")
+    public void rechargeMinerFee(){
+        AccountReplay accountReplay = new AccountReplay(accountService,100);
+        accountReplay.run(account -> {
+            logger.info("process account:{}",account);
+            try {
+                //查询余额
+                BigDecimal ethBalance = ethService.getBalance(account.getAddress());
+                //同步账户余额
+                accountService.updateBalance(account.getAddress(),ethBalance);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+    }
+}
